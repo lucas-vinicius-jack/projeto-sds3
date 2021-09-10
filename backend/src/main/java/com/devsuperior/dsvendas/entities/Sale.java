509c6f19ddd169dd863fb6ac78dae11f91604a01
lucas-vinicius-jack <lucasvinicius1657@gmail.com>
2021-09-10 00:28:02 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -30,7 +30,6 @@ public class Sale {
 	}
 
 	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
-		super();
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
@@ -38,6 +37,58 @@ public class Sale {
 		this.date = date;
 		this.seller = seller;
 	}
+
+	// GET E SET --------------------------------------
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public Integer getVisited() {
+			return visited;
+		}
+
+		public void setVisited(Integer visited) {
+			this.visited = visited;
+		}
+
+		public Integer getDeals() {
+			return deals;
+		}
+
+		public void setDeals(Integer deals) {
+			this.deals = deals;
+		}
+
+		public Double getAmount() {
+			return amount;
+		}
+
+		public void setAmount(Double amount) {
+			this.amount = amount;
+		}
+
+		public LocalDate getDate() {
+			return date;
+		}
+
+		public void setDate(LocalDate date) {
+			this.date = date;
+		}
+
+		public Seller getSeller() {
+			return seller;
+		}
+
+		public void setSeller(Seller seller) {
+			this.seller = seller;
+		}
+	
+	
+	
 	
 	
 }
